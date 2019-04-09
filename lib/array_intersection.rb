e3ba5dcc306e20d08c537b3c8dafa86be11f2729
Amy Wyatt <amy.wyatt12@outlook.com>
2019-04-09 13:56:46 -0700

    can calculate array intersection

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,6 +1,23 @@
 # Returns a new array to that contains elements in the intersection of the two input arrays
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) where n is the length of array1
+# Space complexity: O(n) where n is the length of the intersection array
 def intersection(array1, array2)
-  raise NotImplementedError
+  intersection = []
+  return intersection if array1 == nil ||
+                         array2 == nil ||
+                         array1 == [] ||
+                         array2 == []
+
+  i = 0
+  while array1[i] != nil
+    j = 0
+    while array2[j] != nil
+      if array1[i] == array2[j]
+        intersection << array2[j]
+      end
+      j += 1
+    end
+    i += 1
+  end
+  return intersection
 end
